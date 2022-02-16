203a16273b13578ccfebe40629706ce7148b6610
Nihey Takizawa <nihey.takizawa@gmail.com>
2022-02-16 15:06:42 -0300

    Import notion css directly on the pages that need it

--- src/pages/vagas.js
+++ src/pages/vagas.js
@@ -5,6 +5,8 @@ import jobsPageJSON from '../contants/vagas.json';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
+import '../styles/notion.css';
+
 const Jobs = () => (
   <Layout>
     <SEO title="Ventures" meta={[{ property: 'og:url', content: 'https://kassellabs.io/ventures/' }]} />
